e405f13696b6d5412d61d160c622d23313217936
吴晓清 <13640140557@163.com>
2020-05-09 11:41:00 +0800

    SpringCloud Stream消息驱动 屏蔽底层消息中间件的差异，降低切换成本，统一消息的编程模型

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/atguigu/springcloud/StreamMQmMain8802.java
@@ -0,0 +1,9 @@
+package com.atguigu.springcloud;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class StreamMQmMain8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQmMain8802.class,args);
+    }
+}
---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/service/IMessageProvider.java
@@ -0,0 +1,5 @@
+//发送消息接口
+package com.atguigu.springcloud.service;
+public interface IMessageProvider {
+    public String send();
+}
